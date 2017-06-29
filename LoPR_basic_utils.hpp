4f207b2ce6afdcd293f991231308b19c1e1b8ce0
Chip Fay <cwfay4@gmail.com>
2017-06-29 10:26:35 -0400

    graph change commit

---
+++ LoPR_basic_utils.hpp
@@ -0,0 +1,22 @@
+/*** include file for clstrfns.cpp ***/
+
+#ifndef _tp_basic_utils_H_
+#define _tp_basic_utils_H_
+
+#include <cstdio>                                     
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+
+/** macros **/
+
+
+/** types and classes**/
+
+
+/** function prototypes **/
+extern void LoPR_usage(int verbose);
+     
+#endif
